4ec22d462ce2ca3221341f70f90f020cb46bf827
warlock90 <warlock9_0@yahoo.gr>
2013-03-31 20:23:43 +0300

    Created main interface

--- src/com/qrpasswds/MainActivity.java
+++ src/com/qrpasswds/MainActivity.java
@@ -1,20 +1,27 @@
 package com.qrpasswds;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
 	
-	
+	private LinearLayout main = null;
+	private ArrayList<Credentials> list = null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		main = (LinearLayout) findViewById(R.id.main);
+		list = new ArrayList<Credentials>();
 		
 	}
 	
