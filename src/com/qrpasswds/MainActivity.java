5c50f58f8647cad41e7d88247f4d409067e65e7b
warlock90 <warlock9_0@yahoo.gr>
2013-03-31 19:56:46 +0300

    Created Credentials custom view

--- src/com/qrpasswds/MainActivity.java
+++ src/com/qrpasswds/MainActivity.java
@@ -1,11 +1,15 @@
 package com.qrpasswds;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends Activity {
-
+	
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +20,28 @@ public class MainActivity extends Activity {
 	
 	public void onResume(){
 		super.onResume();
-	
+		
 	}
 
+	public void onStop(){
+		super.onStop();
+
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	private class Credentials extends View{
+
+		public Credentials(Context context,ViewGroup viewgroup) {
+			super(context);
+			Credentials.inflate(this.getContext(), R.layout.credentials, viewgroup);
+		}
+		
 	}
 
 }
